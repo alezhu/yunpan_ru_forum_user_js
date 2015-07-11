c971980e016ad8a50ea921b155c380b7aede07a8
Alexandr Zhuravlev <lev-money@mail.ru>
2015-07-11 11:26:26 +0300

    Create yunpan.ru.user.js

---
+++ yunpan.ru.user.js
@@ -0,0 +1,17 @@
+// ==UserScript==
+// @name         yunpan.ru forum
+// @namespace    https://raw.githubusercontent.com/alezhu/yunpan_ru_forum_user_js/
+// @version      1.0
+// @description  Some usefal function for forum
+// @author       alezhu
+// @match        http://yunpan.ru/
+// @include      http://yunpan.ru/
+// @source      https://raw.githubusercontent.com/alezhu/yunpan_ru_forum_user_js/yunpan.ru.user.js
+// @updateURL   https://raw.githubusercontent.com/alezhu/yunpan_ru_forum_user_js/yunpan.ru.user.js
+// @downloadURL https://raw.githubusercontent.com/alezhu/yunpan_ru_forum_user_js/yunpan.ru.user.js
+// @grant        none
+// ==/UserScript==
+
+(function() {
+  
+})();
