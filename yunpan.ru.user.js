0358d89a953c3b332253f917fe90adf2d1e1862f
Alexandr Zhuravlev <lev-money@mail.ru>
2015-07-11 12:07:47 +0300

    Update yunpan.ru.user.js

--- yunpan.ru.user.js
+++ yunpan.ru.user.js
@@ -4,8 +4,8 @@
 // @version      1.0
 // @description  Some usefal function for forum
 // @author       alezhu
-// @match        http://yunpan.ru/
-// @include      http://yunpan.ru/
+// @match        http://yunpan.ru/*
+// @include      http://yunpan.ru/*
 // @source      https://raw.githubusercontent.com/alezhu/yunpan_ru_forum_user_js/yunpan.ru.user.js
 // @updateURL   https://raw.githubusercontent.com/alezhu/yunpan_ru_forum_user_js/yunpan.ru.user.js
 // @downloadURL https://raw.githubusercontent.com/alezhu/yunpan_ru_forum_user_js/yunpan.ru.user.js
